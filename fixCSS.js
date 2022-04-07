775fb04c0e458c7ad137d4bc1519d666d5c57c93
alfarley01@gmail.com <andrewfarley@Andrew-MBP.localdomain>
2022-04-07 10:10:43 -0700

    Add listener function for getElements. Not yet functioning.

--- fixCSS.js
+++ fixCSS.js
@@ -4,3 +4,13 @@ for(let i=0; i < 10; i++){
     document.getElementsByClassName("item-list-container")[i].style.overflowY = 'scroll'
     document.getElementsByClassName("integration-page")[i].style.overflowY = 'scroll'
 }
+
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(){
+    for(let i=0; i < 10; i++){
+        document.getElementsByClassName("vertical-list")[i].style.overflowY = 'scroll'
+        document.getElementsByClassName("item-list")[i].style.overflowY = 'scroll'
+        document.getElementsByClassName("item-list-container")[i].style.overflowY = 'scroll'
+        document.getElementsByClassName("integration-page")[i].style.overflowY = 'scroll'
+    }
+})
