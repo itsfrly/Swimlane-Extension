3f334b6c8ceaac3db43fe8a428850b859a29e655
alfarley01@gmail.com <andrewfarley@Andrew-MBP.localdomain>
2022-04-07 09:03:03 -0700

    Fix a missing var 'i' within for loop

--- fixCSS.js
+++ fixCSS.js
@@ -1,6 +1,6 @@
 for(let i=0; i < 10; i++){
     document.getElementsByClassName("vertical-list")[i].style.overflowY = 'scroll'
-    document.getElementsByClassName("item-list")[0].style.overflowY = 'scroll'
-    document.getElementsByClassName("item-list-container")[0].style.overflowY = 'scroll'
-    document.getElementsByClassName("integration-page")[0].style.overflowY = 'scroll'
+    document.getElementsByClassName("item-list")[i].style.overflowY = 'scroll'
+    document.getElementsByClassName("item-list-container")[i].style.overflowY = 'scroll'
+    document.getElementsByClassName("integration-page")[i].style.overflowY = 'scroll'
 }
